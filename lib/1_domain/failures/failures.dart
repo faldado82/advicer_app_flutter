175251eaf3faa082705637fb5e52b5db7a42e400
faldado82 <faldado82@gmail.com>
2023-06-27 17:01:17 -0300

    unit testing in repositories completed successfully

--- lib/1_domain/failures/failures.dart
+++ lib/1_domain/failures/failures.dart
@@ -1,9 +1,18 @@
-abstract class Failure{
+import 'package:equatable/equatable.dart';
 
+abstract class Failure {}
+
+class ServerFailure extends Failure with EquatableMixin{
+  @override
+  List<Object?> get props => [];
 }
 
-class ServerFailure extends Failure{}
+class CacheFailure extends Failure with EquatableMixin{
+  @override
+  List<Object?> get props => [];
+}
 
-class CacheFailure extends Failure{}
-
-class GeneralFailure extends Failure{}
+class GeneralFailure extends Failure with EquatableMixin{
+  @override
+  List<Object?> get props => [];
+}
